b4551432cfa2d2ee01666984795541c77c6f86e5
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-01-20 12:12:57 -0300

    feat: create global styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
 import { App } from './App'
+import { GlobalStyle } from 'styles/global'
 import { theme } from 'styles/theme'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <App />
+    <GlobalStyle />
   </ThemeProvider>,
   document.querySelector('#root'),
 )
