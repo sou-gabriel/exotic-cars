e256087f45ace8fb3f45e69bc2845c3e1a7d022b
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-01-24 23:01:26 -0300

    refactor: restructure car data format

--- src/components/CarItem/index.tsx
+++ src/components/CarItem/index.tsx
@@ -1,3 +1,5 @@
+import { Car } from 'context/CarsContext'
+
 import {
   Container,
   Header,
@@ -9,39 +11,34 @@ import {
   Price,
 } from './styles'
 
-type Props = {
-  car: {
-    brand: string
-    model: string
-    image: string
-    pricePerDay: number
-  }
+type CartItemProps = {
+  car: Car
 }
 
-export const CarItem = ({ car }: Props) => {
+export const CarItem = ({ car }: CartItemProps) => {
   return (
     <Container>
-      <Header>
-        <div>
-          <Brand>{car.brand}</Brand>
-          <Model>{car.model}</Model>
-        </div>
+        <Header>
+          <div>
+            <Brand>{car.brand.name}</Brand>
+            <Model>{car.model}</Model>
+          </div>
 
-        <DetailsButton>
-          <Bullet />
-          <Bullet />
-          <Bullet />
-        </DetailsButton>
-      </Header>
+          <DetailsButton>
+            <Bullet />
+            <Bullet />
+            <Bullet />
+          </DetailsButton>
+        </Header>
 
-      <Image src={car.image} alt={`${car.brand} - ${car.model}`} />
+        <Image src={car.image} alt={`${car.brand.name} - ${car.model}`} />
 
-      <Price>
-        <strong>
-          <sup>$</sup>{car.pricePerDay}
-        </strong>
-        <sub>/day</sub>
-      </Price>
+        <Price>
+          <strong>
+            <sup>$</sup>{car.pricePerDay}
+          </strong>
+          <sub>/day</sub>
+        </Price>
     </Container>
   )
 }
